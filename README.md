adc55a4c7d9ed7054f08d0d4cadff23eef1d9d9e
ChinsenMR <2645800450@qq.com>
2020-05-30 12:16:28 +0800

    添加自述文件

---
+++ README.md
@@ -0,0 +1,12 @@
+  /**
+   * 文件上传 （支持多文件，多类型）
+   * @param {*} options
+   * @returns
+   * 调用方式
+   *  app.tools.upload({
+      type: "image",
+      url: "/AppShop/AppShopHandler.ashx?action=AppUploadImage",
+    }).then(res => {
+      console.log(res)
+    })
+   */
--- app/utils/tools.js
+++ app/utils/tools.js
@@ -168,18 +168,7 @@ module.export = {
     });
   },
 
-  /**
-   * 文件上传 （支持多文件，多类型）
-   * @param {*} options
-   * @returns
-   * 调用方式
-   *  app.tools.upload({
-      type: "image",
-      url: "/AppShop/AppShopHandler.ashx?action=AppUploadImage",
-    }).then(res => {
-      console.log(res)
-    })
-   */
+  /*上传文件 */
   async upload(options) {
     const {
       url,
