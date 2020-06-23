a94f8da405a08ea8e3d61037a0b8f8573e3f9577
Chinsen <2645800450@qq.com>
2020-06-23 20:30:51 +0800

    新增中间件

--- app/utils/mixins/middleware.js
+++ app/utils/mixins/middleware.js
@@ -1,6 +1,6 @@
 module.exports = {
     /* 阻止连续点击 */
-    async beMask() {
+    async setDisable() {
         let timeout = setTimeout(() => {
             clearTimeout(timeout)
             return true
@@ -13,17 +13,47 @@ module.exports = {
     /* 提交在loading出来之前连续点了两次，用此方法解决 */
 
     /* 阻止连续点击 */
-    disableConfirm() {
+    disableConfirm(time = 300) {
         this.setData({
             confirmOrderIng: true
         })
 
-        let timeout = setTimeout(() => {
+        const timeout = setTimeout(() => {
             this.setData({
                 confirmOrderIng: false
             }, () => {
                 clearTimeout(timeout)
             })
-        }, 1000)
+        }, time)
     },
+
+    /**
+     * 解决问题，针对所有需要登陆的操作
+     * 进入某页面 收藏 ——>未登录 ——> 登录 ——> 收藏 ——> 回到收藏页面
+     * 
+     * @param {方法名， 字符串} name 
+     * @param {} options 
+     */
+    handleBeforeAction(name, options) {
+        app.globalData.action = {
+            name,
+            options
+        };
+    },
+    handleAfterAction() {
+        const app = getApp();
+        const pages = getCurrentPages();
+        const targetPage = pages[pages.length - 2];
+
+        const {
+            globalData: {
+                name,
+                options
+            }
+        } = app;
+
+        targetPage[name](options);
+    },
+
+
 }
